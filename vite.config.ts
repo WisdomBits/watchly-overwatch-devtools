5e0f0435e29fb436a47bdd7ff39bb9278766f89c
Yogesh Yadav <yogesh7132@gmail.com>
2025-07-02 01:13:57 +0530

    refactor: add static site support

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,25 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-  root: '.',
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'manifest.json',
+          dest: '.' // copy to dist/
+        },
+        {
+          src: 'public/icon.jpeg',
+          dest: 'public' // optional: copy icons
+        }
+      ]
+    })
+  ],
   build: {
     outDir: 'dist',
     emptyOutDir: true,
-  },
-  plugins: [react()],
+  }
 });
